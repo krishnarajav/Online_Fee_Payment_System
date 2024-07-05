9ad9023d393bb1aafddf0b59aadc2475e3b30725
krishnarajav <118679169+krishnarajav@users.noreply.github.com>
2024-07-05 12:51:12 +0530

    updated files

--- payment_system/studentportal/views.py
+++ payment_system/studentportal/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import datetime
 
 def dashboard(request):
-    return render(request, 'studentportal/student_dash.html')
+    today = datetime.datetime.now()
+    current_date = today.strftime('%d-%m-%Y')
+    return render(request, 'studentportal/student_dash.html', {'current_date' : current_date})
