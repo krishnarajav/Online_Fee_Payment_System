221ddb1889df8aee8dbd189c21bfe8830391a0c2
Mahesh nayak <maheshnire321@gmail.com>
2024-07-06 20:21:52 +0530

    updated functionality for admin_page

--- payment_system/adminportal/urls.py
+++ payment_system/adminportal/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload/', views.addStudent, name='addStudent'),
+    path('getCourse/', views.course, name='course'),
+    path('studentData/', views.studentDetails, name='studentDetails'),
 ]
